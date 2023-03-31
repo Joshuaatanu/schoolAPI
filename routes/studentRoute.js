74f52c62a04a867ba6c5dd35a676711e6a1aa04e
Joshua Atanu <atanu.joshua@gmail.com>
2023-03-31 22:27:43 +0100

    finished the student route

--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -1,6 +1,20 @@
-const express = require('express');
-const router = express.Router()
-const {getStudent} = require('../controllers/studentController')
+const express = require("express");
+const router = express.Router();
+const {
+  getStudent,
+  getStudentById,
+  createStudent,
+  updateStudent,
+  deleteStudent,
+} = require("../controllers/studentController");
+const logger = require("../middleware/logger");
 
- 
-router.route("/students",).get(getStudent)
+router.use(logger);
+
+router.get("/", getStudent);
+router.get("/:id", getStudentById);
+router.post("/", createStudent);
+router.put("/:id", updateStudent);
+router.delete("/:id", deleteStudent);
+
+module.exports = router;
